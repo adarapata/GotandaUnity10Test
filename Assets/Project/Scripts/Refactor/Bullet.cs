17793ef3f492855b4e07d9b40bc9549625fc6afb
adarapata <adarapata@e-f-b.jp>
2019-01-22 03:04:44 +0900

    first commit

---
+++ Assets/Project/Scripts/Refactor/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Vector3 _direction;
+    [SerializeField]
+    private float _speed = 0F;
+
+    public void SetUp(Vector2 direction)
+    {
+        _direction = direction;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += _direction.normalized * _speed * Time.deltaTime;
+    }
+
+    void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Project/Scripts/Refactor/PlayerUnit.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerUnit : MonoBehaviour
+{
+    [SerializeField]
+    private Bullet _bulletPrefab = null;
+    [SerializeField]
+    private float _interval;
+    [SerializeField]
+    private float _speed;
+
+    private float _reloadTime;
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        transform.position += new Vector3(horizontal, vertical, 0F) * _speed * Time.deltaTime;
+
+        if (Input.GetButton("Fire1") && _reloadTime < 0)
+        {
+            var bullet = Instantiate(_bulletPrefab, transform.position, Quaternion.identity);
+            bullet.SetUp(Vector2.up);
+            _reloadTime = _interval;
+        }
+
+        _reloadTime -= Time.deltaTime;
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Bullet")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
